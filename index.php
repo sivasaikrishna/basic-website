fc2cd632c8caa9b3c2cbe705b51898be8f63a830
sivasaikrishna <sivasaikrishnassk@gmail.com>
2017-10-18 00:20:26 +0530

    Add files via upload
    basic website version1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <meta http-equiv="x-ua-compatible" content="ie=edge"> -->
+    <title>Happy DIWALI</title>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap /4.0.0-alpha.5/css/bootstrap.min.css">
+    <style type="text/css">
+    	body{
+    		background-color:black;
+    		color:white;  
+    	}
+    	p,pre.greetings{
+    		font-size: 30px;
+    		font-family: 'Lobster', cursive;
+    	}
+    </style>
+</head>
+<body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
+
+	<div class="container">
+		<div style="margin:auto; width: 80% " class="bg-primary">
+			<div>
+			<p class="greetings">This Diwali,<br>Let the firecrackers burn your sorrows,<br>Candles light up your life,<br>Colors make your life bright<br>With flowers filling in the fragrance all day and night,<br>
+			<pre class="greetings">						Happy Diwali	--TEJA</pre></p>
+
+		<br><br>
+			<a href="diwali.png" target="_blank">
+			<img src="diwali.png"  alt="Greetings" style="max-width:100%;"></a>
+		</div>
+
+		<footer><p>Copyright © <?php echo date("Y"); ?> Aero-Maggus. All rights reserved</p></footer>
+		</div>	
+	</div>
+
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+To see the website you must start your local server and open the file from local host or upload this folder into server to see the live version online.
+For any queries contact :
+	TEJA,
+	Admin of Aero-Maggus,
+	Email:aeromaggus@gmail.com 
